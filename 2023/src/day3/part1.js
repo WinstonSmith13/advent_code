5675d51ad4f4e06de18e6ac5fd33d35819466d4b
Medy <krouk.medy@gmail.com>
2023-12-04 00:18:52 +0100

    day3 part1 wip.

--- 2023/src/day3/part1.js
+++ 2023/src/day3/part1.js
@@ -6,18 +6,21 @@ export default class Part1 {
 
     async resolve() {
         const ligns = this.puzzle.split(this.line);
+        
         const lines = ligns.map(line =>
             line.trim().replace(/\./g, '.'));
-    
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
+
+        console.log(ligns)
+        console.log(lines)
+
+        for(let i = 0; i < lines.length; i++){
+
+            const line = lines[i]
             console.log(`Line ${i + 1}: ${line}`);
-            console.log(`Length: ${line.length}`);
-    
-            // Obtenir les index de chaque élément dans la ligne
-            for (let j = 0; j < line.length; j++) {
-                console.log(`Element at index ${j}: ${line[j]}`);
+            for(let l = 0; l < line.length; l++){
+                console.log(`index ${l}`,line[l])
             }
         }
+     
     }
 }
