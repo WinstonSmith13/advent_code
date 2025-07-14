758190eab7306450ad92489caa4acc47c5221505
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-07-14 22:41:09 +0200

    Day1: part 1 done.

--- 2024/src/day1/puzzle1.js
+++ 2024/src/day1/puzzle1.js
@@ -1,3 +1,35 @@
 export function solve(input){
-  return input.split('\n').length;
+  const lines = input.trim().split('\n');
+  const columnLeft = [];
+  const columnRight = [];
+
+  lines.forEach(line => {
+
+    const parts = line.trim().split(/\s+/);
+
+    parts.forEach((part, i) => {
+      if(i%2 === 0){
+        columnLeft.push(part);
+      } else columnRight.push(part);
+
+    })
+  });
+  
+  columnLeft.sort((a,b) => a-b);
+  columnRight.sort((a,b) => a-b);
+
+  const diff = [];
+
+  for (let i=0; i<columnLeft.length; i++){
+    const left = columnLeft[i];
+    const right = columnRight[i];
+  
+    diff.push(Math.abs(left-right));
+  }
+
+
+  const sommeDiff = diff.reduce((acc, val) => acc + val, 0);
+
+
+  return sommeDiff;
 }
