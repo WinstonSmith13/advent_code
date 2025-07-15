c7496ac042df62ef1959a091ab52952efa468f1e
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-07-15 20:44:50 +0200

    day1: part1 done.

--- 2024/src/day2/puzzle1.js
+++ 2024/src/day2/puzzle1.js
@@ -2,18 +2,36 @@ export function solve(input){
   const lines = input.trim().split('\n');
   const reports = lines.map(line => line.trim().split(' ').map(Number));
 
+  let safetyReports = [];
   
-let safetyReports = [];
-
   reports.forEach((e, index) => {
-  console.log("Ligne", index, ":", e);
 
-  for (let j = 0; j < e.length - 1; j++) {
-    let safetyScore = e[j + 1] - e[j];
-    console.log("Diff entre", e[j + 1], "et", e[j], "=", safetyScore);
-  }
-});
+    let diffs = [];
 
-console.table(safetyReports);
-  return reports;
+    for (let j = 0; j < e.length - 1; j++) {
+      let safetyScore = e[j + 1] - e[j];  
+      diffs.push(safetyScore);
+    }
+
+    safetyReports.push({line: index, reports: e, differences:diffs});
+  });
+
+  safetyReports.forEach((report, index)=>{
+ 
+    report.isSafe = true;
+   
+      if (report.differences.includes(0)) {
+        report.isSafe = false;
+        return;
+      }
+      if(!report.differences.every(d=> d<0) && !report.differences.every(d=> d>0) ){
+        report.isSafe = false;
+        return;
+      }
+      report.isSafe = report.differences.every(d=>Math.abs(d)>=1 && Math.abs(d)<=3);
+  });
+ 
+  const countSafe = safetyReports.filter(reportObject => reportObject.isSafe === true).length;
+ 
+  return countSafe;
 }
