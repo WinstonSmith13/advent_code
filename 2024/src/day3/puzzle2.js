d1f25fba0df38ad407d4458ca31862db864a37ef
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-07-20 13:26:44 +0200

    day3: part1 todo.

---
+++ 2024/src/day3/puzzle2.js
@@ -0,0 +1,10 @@
+export function solve(input){
+  const lines = input.trim().split('\n');
+  const reports = lines.map(line => line.trim().split(' ').map(Number));
+
+  let safetyReports = [];
+  
+  
+ 
+  return 'Solution part2';
+}
