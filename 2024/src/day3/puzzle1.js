6ae5aeb37fb69fa2c3bcc700cf7bccf1fd3bed4d
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-08-05 13:22:15 +0200

    day3: part1 done

--- 2024/src/day3/puzzle1.js
+++ 2024/src/day3/puzzle1.js
@@ -1,16 +1,6 @@
 export function solve(input){
-
-  const matches = input.matchAll(/mul\((\d+),(\d+)\)/g);
-
-  
-  for (const m of matches) {
-  //console.log(m[0]);
-  //console.log(m[1]);
-  //console.log(m[2]);
-  //console.log('m', m);
-  console.log(JSON.stringify(m, null, 2));
+  return Array.from(input.matchAll(/mul\((\d+),(\d+)\)/g))
+  .map(m => Number(m[1]) * Number(m[2]))
+  .reduce((acc, val) => acc + val, 0);
 }
 
- 
-  return 'result';
-}
