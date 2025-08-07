38bbc6a57ed6c7ab12ee442181ac020f128f8d08
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-08-07 11:49:11 +0200

    day4: part 1 todo.

--- 2024/src/day3/puzzle2.js
+++ 2024/src/day3/puzzle2.js
@@ -1,27 +1,24 @@
-export function solve(input){
+export function solve(input) {
 
   const regex = /(?<type>do\(\)|don't\(\)|mul\((\d+),(\d+)\))/g;
   const matches = [...input.matchAll(regex)];
-  
+
   let isAllowed = true;
   const results = [];
 
-
-  for (const m of matches){
+  for (const m of matches) {
     if (m.groups.type === "don't()") {
       isAllowed = false;
     } else if (m.groups.type === "do()") {
       isAllowed = true;
-    }
-     else if (m.groups.type.startsWith('mul(')){
-      if (isAllowed){
-        const num1 = Number(m[2]);
-        const num2 = Number(m[3]);
-        const product = num1 * num2;
-        results.push(product);
-      }
+    } else if (isAllowed) {
+      const num1 = Number(m[2]);
+      const num2 = Number(m[3]);
+      const product = num1 * num2;
+      results.push(product);
     }
   }
+
   const total = results.reduce((acc, val) => acc + val, 0);
   return total;
 }
---
+++ 2024/src/day4/puzzle1.js
@@ -0,0 +1,6 @@
+export function solve(input){
+  return 'result';
+}
+
+
+
