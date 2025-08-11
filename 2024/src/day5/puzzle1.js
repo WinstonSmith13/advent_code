2a867ecf36ad16be042a2aae9dbd4aa65ad909d6
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-08-11 12:24:13 +0200

    day5: part1 wip

--- 2024/src/day5/puzzle1.js
+++ 2024/src/day5/puzzle1.js
@@ -1,12 +1,26 @@
 export function solve(input){
-  const [first_section, second_section] = input
+  const [first, second] = input
     .trim()
     .split(/\r?\n\r?\n/)
     .map(part => part.split(/\r?\n/));
 
+  const first_section = first.map(line => line.trim().split(/\|/));
+  console.log("first_section_clean :", first_section);
 
-  console.log("first_section :", first_section);
-  console.log("second_section :", second_section);
+  const second_section = second.map(line => line.trim().split(/,/));
+  console.log("second_section_clean :", second_section);
+
+  // Faire une fonction pour tester 
+  // Verifier l'ordre des numeros ligne par ligne
+  // Faire un tableau d'ordrer des numeros
+  // Comparer les tableaux 
+
+  for (let i = 0; i < first_section.length; i++) {
+    console.log("first_section[i] :", first_section[i]);
+
+  }
+
+
 
   const counter = 0;
 
