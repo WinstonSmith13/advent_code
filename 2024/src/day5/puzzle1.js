dd99e87236452bc9420f58f814ac9ef74f5f9d39
Medy Krouk <medy.krouk@digitalvirgo.com>
2025-08-11 11:57:19 +0200

    day5: part1 wip.

--- 2024/src/day5/puzzle1.js
+++ 2024/src/day5/puzzle1.js
@@ -1,8 +1,13 @@
 export function solve(input){
-  const lines = input.trim().split('\n');
-  const letters = lines.map(line => line.trim().split(''));
-  console.log(lines);
-  console.log(letters);
+  const [first_section, second_section] = input
+    .trim()
+    .split(/\r?\n\r?\n/)
+    .map(part => part.split(/\r?\n/));
+
+
+  console.log("first_section :", first_section);
+  console.log("second_section :", second_section);
+
   const counter = 0;
 
 
